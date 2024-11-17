e86e0bc1ac2a42b24fc4546d4317e395421868cd
jjh1026 <79396331+jjh1026@users.noreply.github.com>
2024-11-17 20:38:29 +0900

    Update MjpegStreamController.java

--- web-develop/src/main/java/capstone/web/test/MjpegStreamController.java
+++ web-develop/src/main/java/capstone/web/test/MjpegStreamController.java
@@ -3,6 +3,7 @@ package capstone.web.test;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
@@ -14,11 +15,12 @@ import java.net.URLConnection;
 @Controller
 public class MjpegStreamController {
 
-    private final String MJPEG_STREAM_URL = "http://182.209.79.20:5000/video_feed"; // Flask MJPEG 스트리밍 URL
+    private  String MJPEG_STREAM_URL; // Flask MJPEG 스트리밍 URL
 
     // 환영 메시지와 비디오 스트리밍을 포함한 HTML 페이지 제공
     @GetMapping("/")
-    public String welcomePage() {
+    public String welcomePage(@RequestParam("jetsonIp") String jetsonIp) {
+        MJPEG_STREAM_URL = "http://" + jetsonIp + ":5000/video_feed";;
         return "index";  // templates/index.html 페이지 반환
     }
 
@@ -26,7 +28,7 @@ public class MjpegStreamController {
     @GetMapping("/video-feed")
     @ResponseBody
     public void streamVideo(HttpServletResponse response) throws IOException {
-        // URL 연결 (Flask 서버에서 MJPEG 스트리밍을 받아옴)
+
         System.out.println("MjpegStreamController.streamVideo");
         URL url = new URL(MJPEG_STREAM_URL);
         URLConnection connection = url.openConnection();
